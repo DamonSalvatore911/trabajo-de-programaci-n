b93b45845ec5f5b67e6fea2f55c1b7c8745052f7
DamonSalvatore911 <jfuquenet@unal.edu.co>
2024-08-19 19:25:34 -0500

    Create Valores_ascendentes_diccionario.py

---
+++ Valores_ascendentes_diccionario.py
@@ -0,0 +1,7 @@
+# Ejemplo
+cars = {'ferrari': 488,'porsche': 911,'mclaren': 765,'aston': 77}
+
+# Ordenar y imprimir los valores de forma ascendente
+valores_ordenados = sorted(cars.values())
+for valor in valores_ordenados:
+    print(valor)
